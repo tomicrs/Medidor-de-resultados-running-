7bb7ab80dfbaace6efcfc9bc418766a5a621772b
tomicarrasco <tomicarras16@gmail.com>
2024-07-08 20:17:17 -0300

    agregando objetos y clases

--- main.js
+++ main.js
@@ -1,59 +1,113 @@
-let nombre = prompt("Atleta, ingrese su nombre");
-alert("¡Bienvenido "+ nombre + "!");
+// Funciones de validación de entrada
 
-let kms = Number(prompt("Ingrese la distancia en kms que recorrió"));
+function pedirDistancia() {
 
-while(kms == '' || kms == 0){
-    alert("Cantidad de kilometros erronea");
-    kms = Number(prompt("Reingrese la distancia en kms que recorrió"));
-}
+    let kms;
 
-let hs = Number(prompt("Ingrese la cantidad de horas que le tomo realizar el recorrido"));
-    
-if(hs == ''){
-    hs = 0;
-}
+    do {
 
-let min = Number(prompt("Ingrese la cantidad de minutos que le tomo realizar el recorrido"));
+        kms = Number(prompt("Ingrese la distancia en kms que recorrió"));
 
-if(min == ''){
-    min = 0;
-}
-    
+        if (kms <= 0 || isNaN(kms)) {
 
-let seg = Number(prompt("Ingrese la cantidad de segundos que le tomo realizar el recorrido"));
+            alert("Cantidad de kilómetros errónea, por favor ingrese un valor válido.");
 
-if(seg == ''){
-    seg = 0;
-}
+        }
 
-let tiempoTotalEnSegundos = ((hs*3600) + (min*60) + seg);
+    } while (kms <= 0 || isNaN(kms));
 
-while(tiempoTotalEnSegundos == 0){
-    alert("El tiempo total no puede ser 0, reingrese los datos");
-    hs = Number(prompt("Reingrese la cantidad de horas"));
-    min = Number(prompt("Reingrese la cantidad de minutos"));
-    seg = Number(prompt("Reingrese la cantidad de segundos"));
-    tiempoTotalEnSegundos = ((hs*3600) + (min*60) + seg);
-}
-
-function kmsToMetros(){
     return kms*1000;
+
 }
 
+function pedirTiempo() {
 
-let velocidadMS = Math.round(kmsToMetros()/tiempoTotalEnSegundos);  //esta sería la velocidad expresada en m/s.
+    let tiempoTotalEnSegundos = 0;
 
-alert("Velocidad promedio en metros por segundo: " + velocidadMS + "m/s");
+    while (tiempoTotalEnSegundos === 0) {
 
-function velocidadKMH(velocidadMS){ //esta sería la velocidad expresada en km/h.
-    return velocidadMS * 3.6;
+        let hs = Number(prompt("Ingrese la cantidad de horas que le tomó realizar el recorrido")) || 0;
+
+        let min = Number(prompt("Ingrese la cantidad de minutos que le tomó realizar el recorrido")) || 0;
+
+        let seg = Number(prompt("Ingrese la cantidad de segundos que le tomó realizar el recorrido")) || 0;
+
+        tiempoTotalEnSegundos = (hs * 3600) + (min * 60) + seg;
+
+        if (tiempoTotalEnSegundos === 0) {
+
+            alert("El tiempo total no puede ser 0, reingrese los datos.");
+
+        }
+
+    }
+
+    return tiempoTotalEnSegundos;
+
 }
-alert("Velocidad promedio en kilómetros por hora: " + velocidadKMH(velocidadMS) + "km/h");
 
-function velocidadMPH(velocidadMS){
-    return velocidadMS * 2.237;
+class atleta{
+    constructor(nombre, apellido, distancia, tiempo){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.distancia = distancia;
+        this.tiempo = tiempo;
+        this.velocidadPromedio;
+        this.velocidadKMH;
+        this.velocidadMPH;
+    }
+    imprimirAtleta() {
+        return `Nombre: ${this.nombre} Apellido: ${this.apellido} Distancia:${this.distancia} Tiempo:${this.tiempo}`;
+    }
+    velocidadPromedio() {
+        return (this.velocidadPromedio = Math.round(this.distancia / this.tiempo));
+    }
+    velocidadKMH(){
+        return (this.velocidadKMH = Math.round(this.distancia / this.tiempo) * 3.6);
+    }
+    velocidadMPH(){
+        return (this.velocidadKMH = Math.round((this.distancia / this.tiempo) * 2.2));
+    }
+   
 }
-alert("Velocidad promedio en millas por hora: "+ velocidadMPH(velocidadMS) + "mph"); //esta sería la velocidad expresada en mph.
 
+// Código principal
 
+function crearAtleta() {
+    const nombre = prompt("Ingresa tu nombre");
+    const apellido = prompt("Ingresa tu apellido");
+    const distancia = pedirDistancia();
+    const tiempo = pedirTiempo();
+
+    return new atleta(nombre, apellido, distancia, tiempo);
+}
+
+const atletas = [];
+
+let cantidad = Number(parseInt(prompt("Ingrese la cantidad de registros de atletas que se van a ingresar")));
+
+ for (let i = 1; i <= cantidad ; i++) {
+    atletas.push(crearAtleta());
+}
+
+console.log(atletas);
+
+for (let i = 0; i < atletas.length; i++) {
+    console.log(atletas[i].velocidadPromedio());
+    console.log(atletas[i].velocidadKMH());
+    console.log(atletas[i].velocidadMPH());
+}
+
+// let kms;
+
+// let tiempoTotalEnSegundos;
+
+//let distanciaMetros = kmsToMetros(kms);
+
+// let velocidadMS = calcularVelocidadMS(distanciaMetros, tiempoTotalEnSegundos);
+
+// alert("Velocidad promedio en metros por segundo: " + velocidadMS + " m/s");
+
+// alert("Velocidad promedio en kilómetros por hora: " + velocidadKMH(velocidadMS) + " km/h");
+
+// alert("Velocidad promedio en millas por hora: " + velocidadMPH(velocidadMS) + " mph")
